07323a23c4d677b3cc0d9d97d253cc563269ddca
Mikeecee1 <mikeecee@hotmail.com>
2026-07-14 22:05:41 +0100

    completed and tested export process

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ DEFAULT_LNG = -2.5879
 DEFAULT_MONTH = "2025-12"
 
 REQUEST_TIMEOUT = 10  # seconds
+
+#crime categories to export to json file for s3 upload
+EXPORT_CATEGORIES = [   
+     "burglary",
+    "violent-crime",
+    "vehicle-crime",
+]
---
+++ export/json_export.py
@@ -0,0 +1,84 @@
+from config import EXPORT_CATEGORIES
+import json
+from pathlib import Path
+
+from database.crud import get_all_crimes
+
+#uses EXPORT_CATEGORIES from config.py to filter out crime categories that are not in the list. This is used when exporting data to a json file for s3 upload.
+
+def transform_record(crime):
+    """
+    Transform a MongoDB crime document into an export record.
+
+    Returns:
+        dict | None
+    """
+
+    if crime["category"] not in EXPORT_CATEGORIES:
+        return None
+
+    return {
+        "_id": str(crime["_id"]),
+        "category": crime["category"],
+        "month": crime["month"],
+        "review_status": crime.get("review_status", "Not Reviewed"),
+
+        "street":
+            crime["location"]["street"]["name"],
+
+        "latitude":
+            crime["location"]["latitude"],
+
+        "longitude":
+            crime["location"]["longitude"]
+    }
+
+
+
+def transform_records(crimes):
+    """
+    Transform a list of MongoDB crime documents into export records.
+
+    Returns:
+        list: List of transformed records.
+    """
+
+    transformed = []
+
+    for crime in crimes:
+
+        record = transform_record(crime)
+
+        if record is not None:
+            transformed.append(record)
+
+    return transformed
+
+
+
+def export_crimes(filename="transformed_crimes.json"):
+    """
+    Export transformed crime records to a JSON file.
+
+    Args:
+        filename (str): Output filename.
+
+    Returns:
+        str: Path to exported file.
+    """
+
+    crimes = get_all_crimes()
+
+    transformed = transform_records(crimes)
+
+    export_path = Path("exports") / filename
+
+    with open(export_path, "w", encoding="utf-8") as file:
+
+        json.dump(
+            transformed,
+            file,
+            indent=4
+        )
+
+    return str(export_path), len(transformed)
